8aa54837c5dba20fa3332932a31d3a2c6bf708d9
Satoshi Yoshikawa <yoshikawa@fairydevices.jp>
2022-01-01 19:09:28 +0900

    update 4-1

--- ch04/4-1/samplecli-builder/src/main.rs
+++ ch04/4-1/samplecli-builder/src/main.rs
@@ -1,27 +1,15 @@
-use clap::{App, Arg};
+use clap::{arg, App};
 
 fn main() {
     let matches = App::new("My RPN program")
         .version("1.0.0")
         .author("Your name")
         .about("Super awesome sample RPN calculator")
-        .arg(
-            Arg::new("formula_file")
-                .about("Formulas written in RPN")
-                .value_name("FILE")
-                .index(1)
-                .required(false),
-        )
-        .arg(
-            Arg::new("verbose")
-                .about("Sets the level of verbosity")
-                .short('v')
-                .long("verbose")
-                .required(false),
-        )
+        .arg(arg!([FILE] "Formulas written in RPN").required(false))
+        .arg(arg!(-v --verbose ... "Sets the level of verbosity").required(false))
         .get_matches();
 
-    match matches.value_of("formula_file") {
+    match matches.value_of("FILE") {
         Some(file) => println!("File specified: {}", file),
         None => println!("No file specified."),
     }
